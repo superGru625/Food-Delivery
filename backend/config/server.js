2c6e49c535f3c6c0ff0b8fd6f5f75b9b0f9405ab
superGru625 <scarabchain@gmail.com>
2022-11-21 23:20:08 -0800

    [Update] completed

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7ff31e26594f4b4fb7afcd74ac13378f'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7d65d225-a747-41e9-b7cf-b6033d044cd4'
+};
